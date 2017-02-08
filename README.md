66cbf65e6a2317fe0bd4036d0fa5527b80871cf6
t1t1 <cha@hanmail2.net>
2017-02-08 11:03:32 +0900

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# mysql-query-tuning-and-optimization
+MySQL 쿼리 튜닝 및 최적화
+
+## link
+
+* tacademy
+* https://tacademy.sktechx.com/front/tacademy/courseinfo/courseInfoDetail.action?courseCode=444
+* by LHM
+
+## index
+
+## content
---
+++ src/main/java/com/t1t1/MysqlQueryTuningAndOptimizationApplication.java
@@ -0,0 +1,12 @@
+package com.t1t1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlQueryTuningAndOptimizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlQueryTuningAndOptimizationApplication.class, args);
+	}
+}
